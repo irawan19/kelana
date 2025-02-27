6fc2806ba312aa8ba1f4f141d3ac9f52dcea457c
Irawan Agung Nugroho <irawan.nugroho@spesolution.com>
2025-02-27 21:37:15 +0700

    update composer

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -40,10 +40,6 @@ class FortifyServiceProvider extends ServiceProvider
                     return $user;
                 }
         });
-        Fortify::createUsersUsing(CreateNewUser::class);
-        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
